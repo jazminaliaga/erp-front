796f80e13e24f36a0a596812eb5561f91af5e105
Iriel Moro <abrilmoro15@gmail.com>
2025-05-18 23:09:16 -0300

    Editando la Apiurl con el endpoint correcto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://erp-back-4xag.onrender.com";
+const apiUrl = "https://erp-back-4xag.onrender.com/api/productos";
 let allProducts = [];
 
 // Función auxiliar para manejar las solicitudes HTTP
